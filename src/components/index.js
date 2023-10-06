3668584f86bc6dffd6dc5e98033a705caec13b08
Nadia <nadiahosisy@gmail.com>
2023-10-06 16:36:39 +0300

    added Footer component in my project

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,14 @@ import Clients from "./services-components/Clients";
 import Service from "./services-components/Service";
 import RaitingCard from "./services-components/RaitingCard";
 import Subscribe from "./services-components/Subscribe";
+import Footer from "./services-components/Footer";
 
-export { SharedLayout, Navbar, Clients, Service, RaitingCard, Subscribe };
+export {
+  SharedLayout,
+  Navbar,
+  Clients,
+  Service,
+  RaitingCard,
+  Subscribe,
+  Footer,
+};
