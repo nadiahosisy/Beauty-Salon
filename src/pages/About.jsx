b83bbe3bba59e2ab0cd2ada078a2cb9ce1803b5a
Nadia <nadiahosisy@gmail.com>
2023-10-07 12:22:17 +0300

    refactorng About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="main-mission-statement-div">
+      <div className="mission-statement-div">
+        <div className="mission-statement-container">
+          <h2 className="header-mission-statement"></h2>
+          <div>
+            <div className="text-mission-statement">
+              At Beauty-Salon-Nadia, our mission is to empower every individual
+              to feel confident and beautiful. We offer a range of beauty
+              services, from haircuts to nail design, to makeup artistry, all
+              delivered with the highest level of professionalism and care. Our
+              commitment to quality, affordability, and customer satisfaction
+              makes us the go-to beauty destination in town.
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <img></img>
+      </div>
+    </div>
+  );
 };
 
 export default About;
