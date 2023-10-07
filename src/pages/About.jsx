674f6f9d55434f3e845c835a0d3df4512310c4c6
Nadia <nadiahosisy@gmail.com>
2023-10-07 12:53:11 +0300

    added style to about page and fix bug

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,24 +2,30 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="main-mission-statement-div">
-      <div className="mission-statement-div">
-        <div className="mission-statement-container">
-          <h2 className="header-mission-statement"></h2>
-          <div>
-            <div className="text-mission-statement">
-              At Beauty-Salon-Nadia, our mission is to empower every individual
-              to feel confident and beautiful. We offer a range of beauty
-              services, from haircuts to nail design, to makeup artistry, all
-              delivered with the highest level of professionalism and care. Our
-              commitment to quality, affordability, and customer satisfaction
-              makes us the go-to beauty destination in town.
+    <div className="main-container-div">
+      <div className="main-mission-statement-div">
+        <div className="mission-statement-div">
+          <div className="mission-statement-container">
+            <h2 className="header-mission-statement"></h2>
+            <div>
+              <div className="text-mission-statement">
+                At Beauty-Salon-Nadia, our mission is to empower every
+                individual to feel confident and beautiful. We offer a range of
+                beauty services, from haircuts to nail design, to makeup
+                artistry, all delivered with the highest level of
+                professionalism and care. Our commitment to quality,
+                affordability, and customer satisfaction makes us the go-to
+                beauty destination in town.
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="image-container">
-        <img className="left-image"></img>
+        <div className="image-container">
+          <img
+            className="left-image"
+            src="./images/beauty-Salon-test.webp"
+          ></img>
+        </div>
       </div>
     </div>
   );
