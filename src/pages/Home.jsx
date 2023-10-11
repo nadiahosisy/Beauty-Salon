0b938014ac99d8b3810d8fbce8ba9ec3b7f0f292
Nadia <nadiahosisy@gmail.com>
2023-10-11 20:51:17 +0300

    added Services comonent in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { Customers, InnerIcons } from "../components";
+import Services from "./Services";
 
 const Home = () => {
   return (
@@ -35,6 +36,7 @@ const Home = () => {
       </div>
       <InnerIcons />
       <Customers />
+      <Services />
     </>
   );
 };
