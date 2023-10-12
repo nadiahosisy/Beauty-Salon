4eb30bd7aa943a3f0968ae0e46368d21b373861b
Nadia <nadiahosisy@gmail.com>
2023-10-12 15:09:27 +0300

    added another div in beneifts component

--- src/components/home-page-component/FeaturesAndBenefits.jsx
+++ src/components/home-page-component/FeaturesAndBenefits.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const FeaturesAndBenefits = () => {
   return (
-    <div className="features-benifts-main-div">
-      <spn></spn>
-      <span>Features & Benefits</span>
-      <span></span>
+    <div className="main-div-container-features-benifts">
+      <div className="features-benifts-main-div">
+        <spn></spn>
+        <span className="text-features-benifits">Features & Benefits</span>
+        <span></span>
+      </div>
     </div>
   );
 };
