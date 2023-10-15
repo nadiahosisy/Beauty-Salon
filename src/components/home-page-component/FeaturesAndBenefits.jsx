3818c98ae975d0ab0ad122aaff378f023389716e
Nadia <nadiahosisy@gmail.com>
2023-10-15 23:16:57 +0300

    fix

--- src/components/home-page-component/FeaturesAndBenefits.jsx
+++ src/components/home-page-component/FeaturesAndBenefits.jsx
@@ -2,32 +2,39 @@ import React from "react";
 
 const FeaturesAndBenefits = () => {
   return (
-    <div className="main-container">
-      {/* First Section */}
-      <div className="section">
-        <h3 className="header">Features</h3>
-        <div className="circle">
-          <div className="dot"></div>
-        </div>
-        <span className="text">Expert Hair Styling</span>
+    <div className="main-div-container-features-benifts">
+      <div className="features-benifts-main-div">
+        <span className="left-side"></span>
+        <div className="text-features-benifits">Features & Benefits</div>
+        <span className="right-side"></span>
       </div>
 
-      {/* Second Section */}
-      <div className="section">
-        <h3 className="header">Benefits</h3>
-        <div className="circle">
-          <div className="dot"></div>
+      <div className="main-container">
+        {/* First Section */}
+        <div className="section">
+          <div className="circle">
+            <div className="dot"></div>
+          </div>
+          <span className="text">Expert Hair Styling</span>
         </div>
-        <span className="text">Another Feature</span>
-      </div>
 
-      <div className="section">
-        <div className="image-container">
-          <img
-            src="./images/herson-rodriguez.webp"
-            alt="Sample"
-            className="image-herson-rodriguez"
-          />
+        {/* Second Section */}
+        <div className="section">
+          <div className="circle">
+            <div className="dot"></div>
+          </div>
+          <span className="text">Another Feature</span>
+        </div>
+
+        {/* Third Section */}
+        <div className="section">
+          <div className="image-container">
+            <img
+              src="./images/herson-rodriguez.webp"
+              alt="Sample"
+              className="image-herson-rodriguez"
+            />
+          </div>
         </div>
       </div>
     </div>
