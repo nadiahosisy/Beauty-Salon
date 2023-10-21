2f689e1ba8ff1afb09a79c25fd7d873137f90658
Nadia <nadiahosisy@gmail.com>
2023-10-21 16:35:52 +0300

    added another div

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,14 +3,16 @@ import React from "react";
 const SignUp = () => {
   return (
     <div className="main-div-signUp-page">
-      <h2 className="header-register">Register</h2>
-      <div className="form-div-register-page">
-        <label className="label-register">Email address *</label>
-        <input className="input-register"></input>
-        <p className="pargraph-register">
-          A link to set a new password will be sent to your email address.
-        </p>
-        <button className="Button-register">Register</button>
+      <div className="center-signup">
+        <h2 className="header-register">Register</h2>
+        <div className="form-div-register-page">
+          <label className="label-register">Email address *</label>
+          <input className="input-register"></input>
+          <p className="pargraph-register">
+            A link to set a new password will be sent to your email address.
+          </p>
+          <button className="Button-register">Register</button>
+        </div>
       </div>
     </div>
   );
