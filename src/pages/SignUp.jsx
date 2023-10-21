9c2a43a58e0544bd4567e184a2bd8f4b921f87cd
Nadia <nadiahosisy@gmail.com>
2023-10-21 16:25:59 +0300

    added divs and pargraph inside SignUp page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="main-div-signUp-page">
+      <h2 className="header-register">Register</h2>
+      <div className="form-div-register-page">
+        <label className="label-register"></label>
+        <input className="input-register"></input>
+        <p className="pargraph-register"></p>
+        <button className="Button-register"></button>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
