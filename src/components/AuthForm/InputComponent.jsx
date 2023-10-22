7053d4c6cb8d560db7a112f44be7c42956f985d6
Nadia <nadiahosisy@gmail.com>
2023-10-22 23:01:38 +0300

    fixed bug

--- src/components/AuthForm/InputComponent.jsx
+++ src/components/AuthForm/InputComponent.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-const InputComponent = ({ label, type, placeholder }) => {
+const InputComponent = ({ label, type, placeholder, value, onChange }) => {
   return (
     <div className="label-input-div">
       <label className="label-register">{label}</label>
-      <input className="input-register" type={type} placeholder={placeholder} />
+      <input
+        className="input-register"
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
     </div>
   );
 };
--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,35 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { InputComponent } from "../components";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Validate the username and password here
+    if (username === "1111" && password === "1111") {
+      console.log("Login successful");
+      // Perform actions for successful login, e.g., redirect to another page
+    } else {
+      console.log("Invalid username or password");
+      // Handle incorrect credentials, e.g., show an error message to the user
+    }
+  };
+
   return (
     <div className="main-div-sign-up-page ">
       <h2 className="header-register">Login</h2>
       <div className="center-signup">
         <div className="form-div-register-page">
-          <InputComponent
-            label={"Username or email address *"}
-            type={"email"}
-            placeholder={"Enter your username or email address"}
-          />
-          <InputComponent
-            label={"Password *"}
-            type={"password"}
-            placeholder={"Enter your password"}
-          />
-          <div className="label-input-div">
-            <label className="label-register">
-              <input type="checkbox" /> Remember me
-            </label>
-          </div>
-          <p className="paragraph-register">
-            <a className="anchor-register" href="/forgot-password">
-              Lost your password?
-            </a>
-          </p>
-          <div className="sign-up-btn-div">
-            <button className="Button-register">Login</button>
-          </div>
+          <form onSubmit={handleSubmit}>
+            <InputComponent
+              label={"Username or email address *"}
+              type={"text"}
+              placeholder={"Enter your username or email address"}
+              value={username}
+              onChange={handleUsernameChange}
+            />
+            <InputComponent
+              label={"Password *"}
+              type={"password"}
+              placeholder={"Enter your password"}
+              value={password}
+              onChange={handlePasswordChange}
+            />
+            <div className="label-input-div">
+              <label className="label-register">
+                <input type="checkbox" /> Remember me
+              </label>
+            </div>
+            <p className="paragraph-register">
+              <a className="anchor-register" href="/forgot-password">
+                Lost your password?
+              </a>
+            </p>
+            <div className="sign-up-btn-div">
+              <button className="Button-register">Login</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
