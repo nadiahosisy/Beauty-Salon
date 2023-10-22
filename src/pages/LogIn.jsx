43c21c882f75c8e69519d7ddc5f10c70d2b515d2
Nadia <nadiahosisy@gmail.com>
2023-10-22 14:46:21 +0300

    added a login page

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,7 +1,44 @@
 import React from "react";
 
-const LogIn = () => {
-  return <div></div>;
+const Login = () => {
+  return (
+    <div className="main-div-signUp-page">
+      <h2 className="header-register">Login</h2>
+      <div className="center-signup">
+        <div className="form-div-register-page">
+          <div className="label-input-div">
+            <label className="label-register">
+              Username or Email address *
+            </label>
+            <input
+              className="input-register"
+              type="text"
+              placeholder="Enter your username or email address"
+            />
+          </div>
+          <div className="label-input-div">
+            <label className="label-register">Password *</label>
+            <input
+              className="input-register"
+              type="password"
+              placeholder="Enter your password"
+            />
+          </div>
+          <div className="label-input-div">
+            <label className="label-register">
+              <input type="checkbox" /> Remember me
+            </label>
+          </div>
+          <p className="paragraph-register">
+            <a href="/forgot-password">Lost your password?</a>
+          </p>
+          <div className="sign-up-btn-div">
+            <button className="Button-register">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default LogIn;
+export default Login;
