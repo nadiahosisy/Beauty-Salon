d89a872350b8d45b316bcdaeca5fd8df9f705383
Nadia <nadiahosisy@gmail.com>
2023-11-02 13:52:16 +0200

    start build a Calender inside UserPage

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { useState } from "react";
+// import { render } from "react-dom";
+import Calendar from "react-calendar";
 
 const UserPage = () => {
+  const [date, setDate] = useState(new Date());
+  const onChane = () => {
+    setDate(date);
+  };
   return <div>UserPage</div>;
 };
+
+// render(<UserPage />), document.querySelector("#root");
 
 export default UserPage;
