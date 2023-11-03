e45da3b309696df11f481b4fa343062d28f93d43
Nadia <nadiahosisy@gmail.com>
2023-11-03 19:26:20 +0200

    add spacing

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -13,6 +13,7 @@ export function AuthProvider({ children }) {
   function signup(email, password) {
     return auth.createUserWithEmailAndPassword(email, password);
   }
+
   function login(email, password) {
     return auth.signInWithEmailAndPassword(email, password);
   }
