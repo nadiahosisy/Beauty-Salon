29e0aa48e8f2069667725d82e65253264e9b2c7b
Nadia <nadiahosisy@gmail.com>
2023-11-07 17:36:06 +0200

    added Scheduler to my project

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,44 +1,80 @@
-import { render } from "react-dom";
 import React, { useState } from "react";
-import Calendar from "react-calendar";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment);
 
 const UserPage = () => {
-  const [date, setDate] = useState(new Date());
+  const [events, setEvents] = useState([]); // State to store appointments
+  const [selectedService, setSelectedService] = useState(null);
   const [selectedDate, setSelectedDate] = useState(null);
 
-  const onChange = (date) => {
-    setDate(date);
+  const handleServiceChange = (e) => {
+    // Handle service selection
+    setSelectedService(e.target.value);
   };
 
-  const handleDateClick = (value) => {
-    setSelectedDate(value);
+  const handleDateChange = (date) => {
+    // Handle date selection
+    setSelectedDate(date);
+  };
+
+  const handleSelectSlot = ({ start, end }) => {
+    // Handle the selected time slot and create a new event
+    if (selectedService && selectedDate) {
+      const newEvent = {
+        start,
+        end,
+        title: `Appointment for ${selectedService} on ${moment(
+          selectedDate
+        ).format("MMMM Do, YYYY")}`,
+      };
+      setEvents([...events, newEvent]); // Add the new event to the events array
+    } else {
+      alert("Please select a service and date before choosing a time slot.");
+    }
   };
 
   return (
-    <div className="user-page-container">
-      <h1 className="header-register">Choose a Date</h1>
-      <div className="form-div-register-page">
-        <Calendar
-          onChange={onChange}
-          value={date}
-          onClickDay={handleDateClick}
-          calendarType="US"
-          locale="en-US"
-          minDate={new Date()}
-          className="calendar-custom-style"
-        />
-        {selectedDate && (
-          <div className="selected-date-info">
-            <h2>Selected Date:</h2>
-            <p>{selectedDate.toDateString()}</p>
-            {/* Additional details or actions related to the selected date */}
-          </div>
-        )}
+    <div className="scheduler-container">
+      <h2>Make an appointment</h2>
+
+      {/* Service selection */}
+      <div className="form-group">
+        <label>Select Service:</label>
+        <select value={selectedService} onChange={handleServiceChange}>
+          <option value="">Select a service</option>
+          <option value="Haircut">Haircut</option>
+          <option value="Manicure">Manicure</option>
+          <option value="Pedicure">Pedicure</option>
+          {/* Add more service options as needed */}
+        </select>
       </div>
+
+      {/* Date selection */}
+      <div className="form-group">
+        <label>Select Date:</label>
+        <input
+          type="date"
+          value={selectedDate}
+          onChange={(e) => handleDateChange(e.target.value)}
+        />
+      </div>
+
+      {/* Calendar for time slot selection */}
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        onSelectSlot={handleSelectSlot}
+        selectable
+        defaultView="week"
+        views={["week", "day"]}
+      />
     </div>
   );
 };
-
-render(<UserPage />, document.querySelector("#root"));
 
 export default UserPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,5 +5,4 @@ import About from "./About";
 import SignUp from "./SignUp";
 import LogIn from "./LogIn";
 import UserPage from "./UserPage";
-
 export { NotFound, Home, Services, About, SignUp, LogIn, UserPage };
