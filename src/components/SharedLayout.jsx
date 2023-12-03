e66c26911ddfb928ef74a6ba0451ce38b8cabf68
Nadia <nadiahosisy@gmail.com>
2023-12-03 12:45:46 +0200

    fix

--- src/components/SharedLayout.jsx
+++ src/components/SharedLayout.jsx
@@ -1,16 +1,11 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+
 import Navbar from "./Navbar";
-import Subscribe from "./services-components/Subscribe";
 
 const SharedLayout = () => {
   return (
     <div>
       <Navbar />
-      {/* <main>
-        <Outlet />
-      </main>
-      <Subscribe /> */}
     </div>
   );
 };
